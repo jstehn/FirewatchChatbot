07741549c8211af0e18ea52c23c7f2e36515539b
Jack Stehn <jack.stehn@berkeley.edu>
2021-06-12 14:46:20 -0700

    Documentation fixes

--- chatbot.py
+++ chatbot.py
@@ -8,19 +8,18 @@ import spacy
 import pickle
 import os
 from collections import deque
-import en_core_web_md
 
 # May need to run beforehand: python -m spacy download en_core_web_md
 
 BOT_RESPONSES = pd.read_json(r"data/responses.json")
 GRAPH_URL = "https://graph.facebook.com/v10.0"
 VERIFY_TOKEN, PAGE_TOKEN = os.environ["VERIFY_TOKEN"], os.environ["PAGE_TOKEN"]
-NLP = en_core_web_md.load()
-# Keep the IDs of the last 10 messages to verify that we haven't responded already.
+NLP = spacy.load("en_core_web_md")
 recent_messages = deque(maxlen=255)
 
 
 class TextVectorizer(TransformerMixin):
+    """Text Vectorizer for intent classification model"""
     def transform(self, X, **transform_params):
         new_X = np.zeros((len(X), NLP.vocab.vectors_length))
         # Iterate over the sentences
@@ -59,7 +58,7 @@ class FacebookChat:
 
     def send_to_messenger(self, ctx):
         """Sends the request to API at the recipient ID
-        
+
         Args:
             ctx (dict) : Dictionary following Facebooks API to send content over messenger
 
@@ -99,12 +98,12 @@ class FacebookChat:
         return self.send_to_messenger(ctx)
 
     def message(self, contents):
-        '''
+        """
         Sends message with quick replies
 
         Paramters:
-            contents (dict) : First element is a string to be sent as a message. Second element is a list of strings for quick replies    
-        '''
+            contents (dict) : First element is a string to be sent as a message. Second element is a list of strings for quick replies
+        """
         split_message = contents["message"].split("\n\n")
         for i in range(len(split_message)):
             ctx = {
@@ -125,6 +124,22 @@ class FacebookChat:
 
 
 def find_response(user_message):
+    """Finds the best response to a user's message:
+    1)  Tries to see if the message is a greeting, goodbye, or thanks.
+    2)  Checks to see if the message is an exact string match to something we
+        already have a response to
+    3)  If there is no match, try to figure out user intent and use that to
+        find a proper response.
+
+    Args:
+        user_message (string): Message that user sent
+
+    Returns:
+        dict:   Dictionary with value of "messsage" as a string and value of
+                "quick_responses" as a list of strings that are used as
+                quick responses.
+    """
+
     fb_nlp = user_message["nlp"]["traits"]
     nlp_proba = {}
     for trait in ("greetings", "bye", "thanks"):
@@ -146,7 +161,7 @@ def find_response(user_message):
         message = BOT_RESPONSES["Response"][message_text]
         links = BOT_RESPONSES["Links"][message_text]
     else:
-        with open(r"data/model.sav", "rb") as file:
+        with open("data/model.sav", "rb") as file:
             MODEL = pickle.load(file)
         probabilities = MODEL.predict_proba([message_text])[0]
         max_proba = max(probabilities)
@@ -170,26 +185,21 @@ def bot_endpoint():
         verify_token = request.GET.get("hub.verify_token")
         hub_challenge = request.GET.get("hub.challenge")
         if verify_token == VERIFY_TOKEN:
-            url = "{0}/me/subscribed_apps?access_token={1}".format(
-                GRAPH_URL, PAGE_TOKEN
-            )
+            url = "{GRAPH_URL}/me/subscribed_apps?access_token={PAGE_TOKEN}"
             response = requests.post(url)
             print("Hub challenge:", hub_challenge)
             return hub_challenge
     else:
-        # Receive the message and update the status to be typing
         body = json.loads(request.body.read())
         user_id = body["entry"][0]["messaging"][0]["sender"]["id"]
         page_id = body["entry"][0]["id"]
         user_message = body["entry"][0]["messaging"][0]["message"]
         message_id = user_message["mid"]
         print(f'Message ID "{message_id}" received.')
-        # Don't respond to messages we've seen before
         if message_id in recent_messages:
             print(f"Already responded; ignoring messaged.")
             return ""
         recent_messages.append(message_id)
-        # Don't respond to our own messages        
         if user_id != page_id:
             chat = FacebookChat(user_id)
             chat.read()
@@ -199,5 +209,4 @@ def bot_endpoint():
         return ""
 
 
-debug(True)
 run(host="0.0.0.0", reloader=True, port=os.environ.get("PORT", "5000"))
